eca431e4d0508fca6c6a59af93873cc52efbfb1e
Phil Nelson <phil@extrafuture.com>
2017-05-07 13:09:55 -0700

    Update create_macos.sh

--- create_macos.sh
+++ create_macos.sh
@@ -17,11 +17,11 @@ cd ../Bin
 
 echo "Removing Old Rassler.app"
 rm -rf Release/$APP_NAME
-rm -rf Release/rassler_macos.zip
+rm -rf Release/$DIST_NAME
 
 echo "Copying New app"
 cp -rf love.app $APP_NAME
-mv appzipped.love Rassler.app/Contents/Resources/SuperGame.love
+mv appzipped.love $APP_NAME/Contents/Resources/SuperGame.love
 mv $APP_NAME Release/
 cd Release
 echo "Zipping up the app"
